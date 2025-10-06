f657f2f1418c783cbbec670eaf10239b8879f630
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-10-06 07:05:43 -0500

    - updated tags definitions - added missing definitions - updated config to correct project

--- dotnet_overview/docusaurus.config.ts
+++ dotnet_overview/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: "https://sreee2001.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/dotnet-azure-overview/",
+  baseUrl: "/docusaurus_dotnet_overview",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "sreee2001", // Usually your GitHub org/user name.
-  projectName: "dotnet-azure-overview", // Usually your repo name.
+  projectName: "docusaurus_dotnet_overview", // Usually your repo name.
 
   onBrokenLinks: "warn",
 
